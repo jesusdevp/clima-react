39f162fe9d97b5a8e177dd947db4a21fbff12957
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-18 14:13:59 -0500

    Verficando resultados y PropTypes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import Clima from "./components/Clima";
+import Error from "./components/Error";
 
 function App() {
   //state formulario
@@ -12,6 +13,7 @@ function App() {
 
   const [consultar, guardarConsultar] = useState(false);
   const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
 
   const { ciudad, pais } = busqueda;
 
@@ -26,10 +28,27 @@ function App() {
 
         guardarResultado(resultado);
         guardarConsultar(false);
+
+        //Detectar si hbu resultados correctos
+        if (resultado.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
       }
     };
     consultarAPI();
+
+    // eslint-disable-next-line
   }, [consultar]);
+
+  let componente;
+
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
 
   return (
     <Fragment>
@@ -44,9 +63,7 @@ function App() {
                 guardarConsultar={guardarConsultar}
               />
             </div>
-            <div className="col m6 s12">
-              <Clima resultado={resultado} />
-            </div>
+            <div className="col m6 s12">{componente}</div>
           </div>
         </div>
       </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Error from "./Error";
 
 const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
   const [error, guardarError] = useState(false);
@@ -30,9 +32,7 @@ const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      {error ? (
-        <p className="red darken-4 error">Todos los campos son obligatorios</p>
-      ) : null}
+      {error ? <Error mensaje="Ambos campos son obligatorios" /> : null}
       <div className="input-field col s12">
         <input
           type="text"
@@ -65,6 +65,12 @@ const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
       </div>
     </form>
   );
+};
+
+Formulario.propTypes = {
+  busqueda: PropTypes.object.isRequired,
+  guardarBusqueda: PropTypes.func.isRequired,
+  guardarConsultar: PropTypes.func.isRequired,
 };
 
 export default Formulario;
