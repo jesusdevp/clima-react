a2c0902feb68d003a69d81c51e0102fd0aa59a30
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-18 11:01:01 -0500

    Header y Formulario creados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
-  return <h1>Clima react</h1>;
+  return (
+    <Fragment>
+      <Header titulo="Clima React" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
