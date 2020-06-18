dc812c9830b81b6bbfa006db7077bac636424a7f
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-18 12:06:30 -0500

    State y validacion del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  //state formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [error, guardarError] = useState(false);
+
+  //extraer ciudad y pais
+  const { ciudad, pais } = busqueda;
+
+  //Funcion que coloca los elementos en el state
+  const handleChange = (e) => {
+    //acutalizar state
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Submit del form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //validar
+    if (ciudad.trim() === "" || pais.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
+      {error ? (
+        <p className="red darken-4 error">Todos los campos son obligatorios</p>
+      ) : null}
       <div className="input-field col s12">
-        <input type="text" name="ciudad" id="ciudad" />
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
         <label htmlFor="ciudad">Ciudad: </label>
       </div>
       <div className="input-field col s12">
-        <select name="pais" id="pais">
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
           <option value="">-- Seleccione un país --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
         </select>
         <label htmlFor="pais">País: </label>
+      </div>
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          value="Buscar Clima"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4 "
+        />
       </div>
     </form>
   );
